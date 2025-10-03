a6d090886f1a4a111fcc92991432c48aa8c61f95
David <david.linkowski@gmail.com>
2025-10-02 20:22:08 -0700

    removed py version of solution in place of C version of solution

---
+++ 7-str-to-int/include/myAtoi.h
@@ -0,0 +1,4 @@
+#ifndef MY_ATOI_H
+#define MY_ATOI_H
+int myAtoi(char* s);
+#endif
---
+++ 7-str-to-int/src/lc007-atoi.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "../include/myAtoi.h"
+
+int main(int argc, char *argv[]) {
+    char *test = myAtoi("42");
+    printf("%s\n", test);
+    return 0;
+}
+
+int myAtoi(char *s) {
+    int rslt = 0;
+    return rslt;
+}
