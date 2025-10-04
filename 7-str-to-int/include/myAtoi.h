b6bec55b94c93771e3fd0639c6337ddc65dfee99
David <david.linkowski@gmail.com>
2025-10-04 15:57:50 -0600

    methods for removing whitespace and leading zeros and determining sign of input

--- 7-str-to-int/include/myAtoi.h
+++ 7-str-to-int/include/myAtoi.h
@@ -1,4 +1,10 @@
 #ifndef MY_ATOI_H
 #define MY_ATOI_H
-int myAtoi(char* s);
+
+int myAtoi(char *s);
+bool isNeg(char *s);
+char* rmLdWhtSpace(char *s);
+char* rmLdZeros(char *s);
+int charToInt(char c);
+
 #endif
--- 7-str-to-int/src/lc007-atoi.c
+++ 7-str-to-int/src/lc007-atoi.c
@@ -1,18 +1,47 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include "../include/myAtoi.h"
 
 int main() {
     printf("\n\nPROGRAM START...\n");
     printf("--------------------\n");
-    int test = myAtoi("2");
+    int test = myAtoi("   -000042yay!");
     printf("\n%d\n", test);
     printf("\n--------------------\n");
     printf("PROGRAM END.\n\n");
     return 0;
 }
 
+bool isNeg(char *s){
+    return *s && *s == '-';
+}
+char* rmLdWhtSpace(char *s){
+    while(*s && *s == ' '){
+        s++;
+    }
+    return s;
+}
+char* rmLdZeros(char *s){
+    while(*s && *s == '0'){
+        s++;
+    }
+    return s;
+}
+
+int charToInt(char c){
+    return (int)c - 48;
+}
+
 int myAtoi(char *s) {
+    s = rmLdWhtSpace(s);
+    bool isNegative = isNeg(s);
+    if(isNegative) s++;
+    s = rmLdZeros(s);
     int rslt = 0;
-    rslt = (int)*s - 48;
+    while(*s && *s >= '0' && *s <= '9'){
+        rslt = rslt * 10 + charToInt(*s);
+        s++;
+    }
+    if(isNegative) rslt = -rslt;
     return rslt;
 }
