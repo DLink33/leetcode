95533ac2c0a8be4144cac8b79c7a55aecbd08430
David <david.linkowski@gmail.com>
2025-10-04 14:24:37 -0700

    Ability to convert a single digit string (0-9)

--- 7-str-to-int/src/lc007-atoi.c
+++ 7-str-to-int/src/lc007-atoi.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include "../include/myAtoi.h"
 
-int main(int argc, char *argv[]) {
-    char *test = myAtoi("42");
-    printf("%s\n", test);
+int main() {
+    printf("\n\nPROGRAM START...\n");
+    printf("--------------------\n");
+    int test = myAtoi("2");
+    printf("\n%d\n", test);
+    printf("\n--------------------\n");
+    printf("PROGRAM END.\n\n");
     return 0;
 }
 
 int myAtoi(char *s) {
     int rslt = 0;
+    rslt = (int)*s - 48;
     return rslt;
 }
