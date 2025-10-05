adfd95ed813f8df16c04dd424b07c59ee3f302f9
David <david.linkowski@gmail.com>
2025-10-04 22:10:25 -0600

    Added function to count the number of digits in a given base 10 int

--- 9-palindrome-num/include/lc009.h
+++ 9-palindrome-num/include/lc009.h
@@ -1,4 +1,5 @@
 #ifndef LC009_H
 #define LC009_H
+int numDigits(int x);
 bool isPalindrome(int x);
 #endif
--- 9-palindrome-num/src/lc009-palidrome-num.c
+++ 9-palindrome-num/src/lc009-palidrome-num.c
@@ -2,8 +2,18 @@
 #include <stdbool.h>
 #include "../include/lc009.h"
 
+unsigned char numDigits(int x) {
+    unsigned char rslt = 1;
+    while (x >= 10) {
+        x /=10;
+        rslt++;
+    }
+    return rslt;
+}
+
 bool isPalindrome(int x) {
     if (x < 0) return false;
+    unsigned char digits = numDigits(x);
     
 }
 
