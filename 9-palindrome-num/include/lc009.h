1f5a23dcdc128e8990c951e1d22db1b5fe906c14
David <david.linkowski@gmail.com>
2025-10-04 22:02:06 -0600

    Starting solution to 009 Palindrome Number

---
+++ 9-palindrome-num/include/lc009.h
@@ -0,0 +1,4 @@
+#ifndef LC009_H
+#define LC009_H
+bool isPalindrome(int x);
+#endif
---
+++ 9-palindrome-num/src/lc009-palidrome-num.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include "../include/lc009.h"
+
+bool isPalindrome(int x) {
+    if (x < 0) return false;
+    
+}
+
+int main() {return 0;}
