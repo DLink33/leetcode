00b4e87bfca92053f8e9f05ae0ad9193cca1bb97
David <david.linkowski@gmail.com>
2025-10-06 20:35:49 -0600

    Added templating for solution functions

---
+++ 10-regex/src/lc010_regex.py
@@ -0,0 +1,17 @@
+#! .venv/bin/python
+
+
+class Solution:
+    def rev(self, i: int, j: int) -> bool:
+        return False
+
+    def isMatch(self, s: str, p: str) -> bool:
+        return False
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
