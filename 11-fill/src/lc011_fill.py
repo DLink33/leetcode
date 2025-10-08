1ed3b1bdf2065108cec1ca9a22def494bf8d623e
David <david.linkowski@gmail.com>
2025-10-08 14:12:43 -0600

    Added O(n) solution with two pointers and added this solution to test

--- 11-fill/src/lc011_fill.py
+++ 11-fill/src/lc011_fill.py
@@ -12,11 +12,24 @@ class Solution:
         j = 0
         while i < length - 1:
             j = i + 1
-            while j <= length - 1:
+            while j < length:
                 currArea = (j - i) * min(height[i], height[j])
                 maxArea = max(maxArea, currArea)
                 j += 1
             i += 1
+        return maxArea
+
+    def maxArea2(self, height: List[int]) -> int:
+        maxArea = 0
+        left = 0
+        right = len(height) - 1
+        while left < right:
+            currArea = min(height[left], height[right]) * (right - left)
+            maxArea = max(maxArea, currArea)
+            if height[left] >= height[right]:
+                right -= 1
+            else:
+                left += 1
         return maxArea
 
 
@@ -24,7 +37,10 @@ def main():
     h = [1, 8, 6, 2, 5, 4, 8, 3, 7]
     smoke = Solution()
     rslt = smoke.maxArea(h)
-    print(rslt)
+    rslt2 = smoke.maxArea2(h)
+    print("expected value: 49\n")
+    print(f"maxArea1: {rslt}")
+    print(f"maxArea2: {rslt2}")
 
 
 if __name__ == "__main__":
--- 11-fill/tests/test_lc011_fill.py
+++ 11-fill/tests/test_lc011_fill.py
@@ -1,34 +1,41 @@
-from typing import List
+from typing import List, Tuple
 
 import pytest
+
 from lc011_fill import Solution
 
 
-@pytest.fixture  # .fixture is used to set up a common test object
+@pytest.fixture
 def sol():
     return Solution()
 
 
-@pytest.mark.parametrize(  # .parametrize is used to run the same test with different inputs
-    "height, expected",
-    [
-        ([1, 8, 6, 2, 5, 4, 8, 3, 7], 49),
-        ([1, 1], 1),
-        ([4, 3, 2, 1, 4], 16),
-        ([1, 2, 1], 2),
-        ([1, 2, 4, 3], 4),
-        ([1, 3, 2, 5, 25, 24, 5], 24),
-        ([2, 3, 4, 5, 18, 17, 6], 17),
-        ([1, 2, 1], 2),
-        ([1, 1], 1),
-        ([1], 0),
-        ([], 0),
-        ([1, 2], 1),
-        ([2, 1], 1),
-        ([1, 3], 1),
-        ([3, 1], 1),
-        ([1] * 1000, 999),  # long array with single height
-    ],
-)
+CASES: List[Tuple[List[int], int]] = [
+    ([1, 8, 6, 2, 5, 4, 8, 3, 7], 49),
+    ([1, 1], 1),
+    ([4, 3, 2, 1, 4], 16),
+    ([1, 2, 1], 2),
+    ([1, 2, 4, 3], 4),
+    ([1, 3, 2, 5, 25, 24, 5], 24),
+    ([2, 3, 4, 5, 18, 17, 6], 17),
+    ([1, 2, 1], 2),
+    ([1, 1], 1),
+    ([1], 0),
+    ([], 0),
+    ([1, 2], 1),
+    ([2, 1], 1),
+    ([1, 3], 1),
+    ([3, 1], 1),
+    ([1] * 1000, 999),
+    ([i for i in range(1000)], 249500),
+]
+
+
+@pytest.mark.parametrize("height, expected", CASES)
 def test_max_area(sol: Solution, height: List[int], expected: int):
     assert sol.maxArea(height) == expected
+
+
+@pytest.mark.parametrize("height, expected", CASES)
+def test_max_area2(sol: Solution, height: List[int], expected: int):
+    assert sol.maxArea2(height) == expected
