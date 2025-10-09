157711c0ef44d3c7bedd9d1835fef3f99563be9d
David Linkowski <david.linkowski@gmail.com>
2025-10-09 13:53:06 -0600

    Added templating for int to roman solution.  Trying different linting and formatting setup for python solution.

--- 12-int-to-roman/src/lc012_intToRoman.py
+++ 12-int-to-roman/src/lc012_intToRoman.py
@@ -2,10 +2,29 @@
 
 
 class Solution:
-    pass
+    def __init__(self):
+        self.roman = (
+            [ (5,'V'), (1, 'I') ],
+            [ (50, 'L'), (10, 'X') ],
+            [ (500, 'D'), (100, 'C') ],
+            [ (1000, 'M') ]
+        )
+    def numDigits(self, num:int) -> int:
+        rslt = 0
+        while (num > 0):
+            num //= 10
+            rslt+=1
+        return rslt
+    def intToRoman(self, num:int) -> str:
+        digits = self.numDigits(num)
+
+        for i in range(digits-1)    
 
 
 def main():
+    sol = Solution()
+    r = sol.numDigits(3749)
+    print(r)
     pass
 
 
