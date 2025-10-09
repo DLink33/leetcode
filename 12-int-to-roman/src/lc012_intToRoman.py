2aa34a38ede4a13929062bae8c1cddd4cbe8d7c1
David Linkowski <david.linkowski@gmail.com>
2025-10-09 15:51:04 -0600

    Starting solution; buggy still

--- 12-int-to-roman/src/lc012_intToRoman.py
+++ 12-int-to-roman/src/lc012_intToRoman.py
@@ -16,15 +16,36 @@ class Solution:
             rslt+=1
         return rslt
     def intToRoman(self, num:int) -> str:
+        roman = self.roman
+        rslt = ""
         digits = self.numDigits(num)
 
-        for i in range(digits-1)    
+        for i in range(digits):
+            for div in roman[digits-(i+1)]:
+                if not ((num // div[0]) == 4 or (num// div[0] == 9)):
+                    numChars = num // div[0]
+                    rslt = rslt + div[1]*numChars
+                    if numChars:
+                        num -= div[0]*numChars
+                elif((num // div[0]) == 4):
+                    rslt += (roman[i][0][1] + roman[i][1][1])
+                    num -= (div[0]*4)
+                    continue
+                else:
+                    rslt += (roman[i][1][1] + roman[i+1][0][1])
+                    num -= (div[0]*9)
+                    continue
+
+                    
+        
+        return rslt
 
 
 def main():
     sol = Solution()
     r = sol.numDigits(3749)
-    print(r)
+    s = sol.intToRoman(3749)
+    print(s)
     pass
 
 
