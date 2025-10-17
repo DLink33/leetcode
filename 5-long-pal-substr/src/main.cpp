d3af010c9b4940f22fb6f1045ea1e5fb714732af
David <david.linkowski@gmail.com>
2025-10-17 15:54:42 -0600

    Just updating main to experiment with some different constructor

--- 5-long-pal-substr/src/main.cpp
+++ 5-long-pal-substr/src/main.cpp
@@ -1,10 +1,22 @@
 #include "leet/solution.hpp"
 #include <memory>
+#include <iostream>
 
 int main() {
+
+    std::string input = "babad";
+
     leet::Solution sol0;
     leet::Solution *sol1 = new leet::Solution();
     auto sol2 = std::make_unique<leet::Solution>();
+
+    std::string rslt0 = sol0.longestPalindrome(input);
+    std::string rslt1 = sol1->longestPalindrome(input);
+    std::string rslt2 = sol2->longestPalindrome(input);
+    std::cout << "Result0: \"" << rslt0 << "\"" << std::endl;
+    std::cout << "Result1: \"" << rslt1 << "\"" << std::endl;
+    std::cout << "Result2: \"" << rslt2 << "\"" << std::endl;
+
     delete sol1;
     return 0;
 }
