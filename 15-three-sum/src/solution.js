653aa09334b4e76b06233162d288d6b467f36f16
David Linkowski <david.linkowski@gmail.com>
2025-10-21 08:26:21 -0600

    Doing naive solution for fun

--- 15-three-sum/src/solution.js
+++ 15-three-sum/src/solution.js
@@ -32,3 +32,13 @@ export var threeSum = function (nums) {
   }
   return rslt;
 };
+
+export var threSumNaive = function (nums) {
+  const rslt = [];
+  const n = nums.length;
+  for (let i = 0; i < n - 2; ++i) {
+    for (let j = i + 1; j < n - 1; ++j) {
+      for (let k = j + 1; k < n; ++k) {}
+    }
+  }
+};
