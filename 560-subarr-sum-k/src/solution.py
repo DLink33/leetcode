c667fdafac4fc3eda588fb92cb5b1b26fd69f327
David <david.linkowski@gmail.com>
2025-10-30 11:54:04 -0600

    Added pytest testing framework for leetcode 560; passing 2/2 tests

--- 560-subarr-sum-k/src/solution.py
+++ 560-subarr-sum-k/src/solution.py
@@ -2,8 +2,8 @@ class Solution:
     def subarraySum(self, nums: list[int], k: int) -> int:
         if not nums:
             return 0
-        
-        prefixMap: dict[int,int] = {}
+
+        prefixMap: dict[int, int] = {}
         prefixMap[0] = 1
         prefixSum: int = 0
         rslt: int = 0
@@ -14,26 +14,27 @@ class Solution:
 
             # if we have found a prefix sum (a running sum at a previous index in the array)
             # whose value is the same as our current running sum minus k (meaning we have a prefix
-            # whose value we can remove from our running sum to get k) add the number of count of 
+            # whose value we can remove from our running sum to get k) add the number of count of
             # that prefix to our return value
-            if (prefixSum-k) in prefixMap:
-                rslt += prefixMap[prefixSum-k]
-            
-            #update our prefix map
+            if (prefixSum - k) in prefixMap:
+                rslt += prefixMap[prefixSum - k]
+
+            # update our prefix map
             if prefixSum in prefixMap:
                 prefixMap[prefixSum] += 1
             else:
                 prefixMap[prefixSum] = 1
-                
+
         return rslt
 
 
 def main():
-    sol:Solution = Solution()
-    nums: list[int] = [1,1,1,1,1,1]
-    k: int = 3
+    sol: Solution = Solution()
+    nums: list[int] = [2, 0, 1, 0, 1]
+    k: int = 2
     rslt: int = sol.subarraySum(nums, k)
     print(rslt)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
