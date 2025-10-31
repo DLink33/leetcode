2edafff52cb69596548a0cf843eccdd717f5f158
David <david.linkowski@gmail.com>
2025-10-31 14:04:01 -0600

    Added solution for removing nth element from the end of a linked list. NOTE: This solultion is contingent on a defined LinkedList object that is not defined here

---
+++ 019-rm-nth-node/src/solution.py
@@ -0,0 +1,34 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def getSize(self, head: Optional[ListNode]) -> int:
+        curr: ListNode = head
+        rslt: int = 0
+        while curr:
+            rslt += 1
+            curr = curr.next
+        return rslt
+
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        size = self.getSize(head)
+
+        curr = head
+        prev = None
+        counter = 0
+
+        while size - counter > n:
+            prev = curr
+            curr = curr.next
+            counter += 1
+        if curr == head:
+            head = curr.next
+            return head
+        prev.next = curr.next
+        return head
