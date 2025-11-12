2897c5151950122a3dba516fabecdf67b5ac13f4
David Linkowski <david.linkowski@gmail.com>
2025-11-12 16:31:36 -0700

    Finished solution; no tests added yet

--- 547-num-provinces/src/Solution.py
+++ 547-num-provinces/src/Solution.py
@@ -2,15 +2,22 @@ from trees.UnionFind import UnionFind
 
 class Solution:
     def findCircleNum(self, isConnected: list[list[int]]) -> int:
-        uf: UnionFind = UnionFind()
-        for i in range(len(isConnected)):
-            uf.find(i)
-        print(uf)
+        n = len(isConnected)
+        uf = UnionFind(range(n))
+        for i in range(n):
+            for j in range(i + 1, n):
+                if isConnected[i][j] == 1:
+                    uf.union(i, j)
+        return len(uf)
+
 
 def main():
-    sol = Solution()
-    isConnected = [[1,0,0],[0,1,0],[0,0,1]]
-    sol.findCircleNum(isConnected=isConnected)
+    # Smoke Test
+    sol:Solution = Solution()
+    isConnected: list[list[int]] = [[1,1,0],[1,1,0],[0,0,1]]
+    rslt: int = sol.findCircleNum(isConnected)
+    print(rslt)
+
 
 if __name__ == '__main__':
     main()
