4f9e85ee19af4bddcbdf8d2ccdf40c1f133e77d7
David Linkowski <david.linkowski@gmail.com>
2025-11-15 17:13:50 -0700

    Created scaffolding for 207 Course Scheudle solution.

---
+++ 207-course-schedule/src/Solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def _course_schedule(self, *args):
+        # Implement your solution here
+        raise NotImplementedError
+
+def main():
+    raise NotImplementedError
+
+if __name__ == '__main__':
+    main()
+  
