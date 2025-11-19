42a5c048634be0452d59ca8f68f12371b78f4bec
David Linkowski <david.linkowski@gmail.com>
2025-11-19 14:34:00 -0700

    Added a 'naive' solution; working on solution with O(n) and no division usage...

--- 238-prod-arr-exp-self/src/Solution.py
+++ 238-prod-arr-exp-self/src/Solution.py
@@ -1,12 +1,22 @@
 class Solution:
-    def _prod_arr_exp_self(self, *args):
-        # Implement your solution here
+    def productExceptSelf(self, nums: list[int]) -> list[int]:
+        
         raise NotImplementedError
+    
+    def productExceptSelf_naive(self, nums: list[int]) -> list[int]:
+        prod:int = 1
+        rslt = [0] * len(nums)
+        for i in range(len(nums)):
+            prod:int = prod * nums[i]
+        for idx, num in enumerate(nums):
+            rslt[idx] = prod//num
+        return rslt
 
 def main():
     sol: Solution = Solution()
-    raise NotImplementedError
-
+    nums: list[int] = [1,2,3,4]
+    print(nums)
+    print(sol.productExceptSelf_naive([1,2,3,4]))
 if __name__ == '__main__':
     main()
   
