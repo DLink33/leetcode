dacc675f85fa24bf89661e4f18572df1b00dd6be
David Linkowski <david.linkowski@gmail.com>
2025-11-21 20:57:18 -0700

    Added py template for 912 sorting array solution

---
+++ 912-sort-arr/src/Solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def _sort_arr(self, *args):
+        # Implement your solution here
+        raise NotImplementedError
+
+def main():
+    sol: Solution = Solution()
+    raise NotImplementedError
+
+if __name__ == '__main__':
+    main()
+  
