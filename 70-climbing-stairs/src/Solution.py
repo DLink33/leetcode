b445c966fb75c2f3266b47e0922bdf3d52d8c5c9
David Linkowski <david.linkowski@gmail.com>
2025-11-26 11:50:23 -0700

    Added solution and pytests; passing 18/18

--- 70-climbing-stairs/src/Solution.py
+++ 70-climbing-stairs/src/Solution.py
@@ -1,12 +1,36 @@
 class Solution:
-    def _climbing_stairs(self, *args):
-        # Implement your solution here
-        raise NotImplementedError
+    def __init__(self):
+        self._ways:dict[int,int] = {1:1, 2:2}
+    def climbStairs(self, n: int) -> int:
+        if n in self._ways:
+            return self._ways[n]
+        rslt: int = self.climbStairs(n-1) + self.climbStairs(n-2)
+        if n not in self._ways:
+            self._ways[n] = rslt
+        return rslt
+    
+    def climbStairsIterative(self, n: int):
+        if n == 1 or n == 2:
+            return n
+        j:int = 1
+        k:int = 2
+        i:int = 3   # Start iat 3 since the first 2 iterations 
+                    # are encapsulated in our early return base cases
+
+        while(i <= n):
+            i+=1
+            nxt:int = j+k  # get the next valute before we start setting j and k
+            j = k # j becomes the current k 
+            k = nxt # k becomes the previous k plus the previous j
+        return k # finally return k when we are done iterating
 
 def main():
     sol: Solution = Solution()
-    raise NotImplementedError
+    n:int = 5
+    ans:int = sol.climbStairs(n)
+    print(ans)
+    ans:int = sol.climbStairsIterative(n)
+    print(ans)
 
 if __name__ == '__main__':
     main()
-  
--- 70-climbing-stairs/tests/test_70_climbing_stairs.py
+++ 70-climbing-stairs/tests/test_70_climbing_stairs.py
@@ -7,10 +7,24 @@ from Solution import Solution
 TestCase = tuple[Any, Any]
 
 TEST_CASES: list[TestCase] = [
-    # Add your test cases here. Common patterns:
-    # (input_value, expected_value)
-    # ((arg1, arg2, ...), expected_value)
-    # ({"args": (a1, ...), "kwargs": {"k": v}}, expected_value)
+    (1, 1),
+    (2, 2),
+    (3, 3),
+    (4, 5),
+    (5, 8),
+    (6, 13),
+    (7, 21),
+    (8, 34),
+    (9, 55),
+    (10, 89),
+    (20, 10946),
+    (30, 1346269),
+    (35, 14930352),
+    (40, 165580141),
+    (45, 1836311903),
+    (50, 20365011074),
+    (60, 2504730781961),
+    (70, 308061521170129)
 ]
 
 
@@ -20,8 +34,8 @@ def test_solution(case_input: Any, expected: Any) -> None:
     if isinstance(case_input, dict) and ("args" in case_input or "kwargs" in case_input):
         args = case_input.get("args", ())
         kwargs = case_input.get("kwargs", {})
-        assert sol.solve(*args, **kwargs) == expected
+        assert sol.climbStairsIterative(*args, **kwargs) == expected
     elif isinstance(case_input, tuple):
-        assert sol.solve(*case_input) == expected
+        assert sol.climbStairsIterative(*case_input) == expected
     else:
-        assert sol.solve(case_input) == expected
+        assert sol.climbStairsIterative(case_input) == expected
