9fa4bd06513e29992d14416ce7a9979b2747a5b5
David Linkowski <david.linkowski@gmail.com>
2025-11-30 13:34:21 -0700

    developed 1D dp solution; added an xlsx file working through the dp table for this problem since it was confusing.

--- 322-coin-change/src/Solution.py
+++ 322-coin-change/src/Solution.py
@@ -1,11 +1,22 @@
 class Solution:
-    def _coin_change(self, *args):
-        # Implement your solution here
-        raise NotImplementedError
+    def coinChange(self, coins: list[int], amount: int) -> int:
+        LARGEST = amount + 1
+        dp = [LARGEST] * (amount+1)
+        dp[0] = 0
+        for i in range (1, amount+1):
+            for c in coins:
+                if c <= i:
+                    dp[i] = min(dp[i], dp[i-c]+1)
+
+        return dp[amount]
+
 
 def main():
     sol: Solution = Solution()
-    raise NotImplementedError
+    amount:int = 6
+    coins:list[int] = [1,3,4]
+    rslt:int = sol.coinChange(coins, amount)
+    print(rslt)
 
 if __name__ == '__main__':
     main()
