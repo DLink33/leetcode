c6b7a6c61a3baebfce3665ce6c97f792828f5b2d
David Linkowski <david.linkowski@gmail.com>
2025-12-01 12:56:19 -0700

    created Python3.11 solution scaffold for 518-coin-changeII

---
+++ 518-coin-changeII/src/Solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def change(self, amount: int, coins: list[int]) -> int:
+        if amount == 0:
+            return 0
+        dp:list[int] = [amount+1] * (amount)
+
+def main():
+    sol: Solution = Solution()
+    raise NotImplementedError
+
+if __name__ == '__main__':
+    main()
+
