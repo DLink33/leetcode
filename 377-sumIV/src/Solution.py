157e26252784446f368101d06d851e680ec7be05
David Linkowski <david.linkowski@gmail.com>
2025-12-03 13:32:19 -0700

    Added a brute force memoization version of the solution for reference and practice.

--- 377-sumIV/src/Solution.py
+++ 377-sumIV/src/Solution.py
@@ -7,11 +7,28 @@ class Solution:
                 if n <= x:
                     dp[x] += dp[x-n]
         return dp[target]
+    
+    def combinationSumIV(self, nums:list[int], target:int) -> int :
+        memo:dict[int,int] = {}
+        def helper(t:int) -> int:
+            if t == 0:
+                return 1
+            rslt = 0
+            for num in nums:
+                if (t-num) in memo: # check to see if we have it memoized already 
+                    rslt += memo[t-num]
+                elif num <= t:
+                    rslt += helper(t-num)
+            memo[t] = rslt # memo our rslt
+            return rslt
+
+        return helper(target)
 
 def main():
     sol: Solution = Solution()
-    nums:list[int] = [9]
-    target:int = 3
+    nums:list[int] = [1,2,3]
+    target:int = 4
+    print(sol.combinationSumIV(nums, target))
     print(sol.combinationSum4(nums, target))
 
 if __name__ == '__main__':
