1abefddd535f2f633fa4b9383b4d77bfa64cba92
David Linkowski <david.linkowski@gmail.com>
2025-12-05 14:21:36 -0700

    added combinatorics solution and a 2D dp solution to problem; passing 16/16 pytests

--- 62-unique-paths/src/Solution.py
+++ 62-unique-paths/src/Solution.py
@@ -1,11 +1,29 @@
+from math import comb
 class Solution:
-    def _unique_paths(self, *args):
-        # Implement your solution here
-        raise NotImplementedError
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp:list[list[int]] = [[0]*(n) for _ in range(m)]
+        dp[0][0] = 1
+        for i in range(m):
+            dp[i][0] = 1
+        for j in range(1, n):
+            dp[0][j] = 1
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i][j-1] + dp[i-1][j]
+        return dp[m-1][n-1]
+    
+    def uniquePaths_choose(self, m: int, n: int) -> int:
+        D:int = m-1
+        R:int = n-1
+        T:int = D + R
+        return comb(T, D)
 
 def main():
     sol: Solution = Solution()
-    raise NotImplementedError
+    m = 3
+    n = 5
+    print(sol.uniquePaths_choose(m,n))
+    print(sol.uniquePaths(m, n))
 
 if __name__ == '__main__':
     main()
--- 62-unique-paths/tests/test_62_unique_paths.py
+++ 62-unique-paths/tests/test_62_unique_paths.py
@@ -7,10 +7,22 @@ from Solution import Solution
 TestCase = tuple[Any, Any]
 
 TEST_CASES: list[TestCase] = [
-    # Add your test cases here. Common patterns:
-    # (input_value, expected_value)
-    # ((arg1, arg2, ...), expected_value)
-    # ({"args": (a1, ...), "kwargs": {"k": v}}, expected_value)
+    ((5, 3), 15),
+    ((3, 2), 3),
+    ((7, 3), 28),
+    ((3, 3), 6),
+    ((10, 10), 48620),
+    ((1, 1), 1),
+    ((2, 2), 2),
+    ((3, 7), 28),
+    ((4, 4), 20),
+    ((5, 5), 70),
+    ((1, 10), 1),
+    ((10, 1), 1),
+    ((2, 10), 10),
+    ((10, 2), 10),
+    ((20, 20), 35345263800),
+    ((100, 100), 22750883079422934966181954039568885395604168260154104734000)
 ]
 
 
@@ -20,8 +32,9 @@ def test_solution(case_input: Any, expected: Any) -> None:
     if isinstance(case_input, dict) and ("args" in case_input or "kwargs" in case_input):
         args = case_input.get("args", ())
         kwargs = case_input.get("kwargs", {})
-        assert sol.solve(*args, **kwargs) == expected
+        assert sol.uniquePaths(*args, **kwargs) == expected
     elif isinstance(case_input, tuple):
-        assert sol.solve(*case_input) == expected
+        assert sol.uniquePaths(*case_input) == expected
+        assert sol.uniquePaths_choose(*case_input) == expected
     else:
-        assert sol.solve(case_input) == expected
+        assert sol.uniquePaths(case_input) == expected  # pyright: ignore[reportCallIssue]
