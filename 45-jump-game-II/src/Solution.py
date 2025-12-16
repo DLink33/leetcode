28f707329854f8e31947c3f0bc157297bc9d1014
David Linkowski <david.linkowski@gmail.com>
2025-12-16 13:57:22 -0700

    Added greedy look-head solution; added pytests: passing 11/11

--- 45-jump-game-II/src/Solution.py
+++ 45-jump-game-II/src/Solution.py
@@ -1,11 +1,29 @@
 class Solution:
-    def _jump_game_II(self, *args):
-        # Implement your solution here
+    def jump(self, nums: list[int]) -> int:
         raise NotImplementedError
+    
+    def jump_greedy(self, nums: list[int]) -> int:
+        n = len(nums)
+        jumps = 0 # tracks the number of jumps we have taken
+        end = 0   # tracks the end of the current range of values we could jump to
+        farthest = 0 # tracks the largest jump value between the current index  and end
+
+        for i in range(n-1):
+            farthest = max(farthest, i + nums[i])
+            if i == end: # we have finished looking at all of the possible indecies to jump to
+                jumps += 1 # now we must jump since we have found the farthest we can get
+                end = farthest # we now have a new end point
+                if end >= n-1: # if we reach the end already we can break and return our jumps
+                    break
+        return jumps
+
 
 def main():
     sol: Solution = Solution()
-    raise NotImplementedError
+    inpt:list[int] = [2,3,1,12,4]   
+    #inpt:list[int] = [1,2,3]
+    rslt = sol.jump_greedy(inpt)
+    print(rslt)
 
 if __name__ == '__main__':
     main()
