d5b7633e9d9019f2529799aed9201a4ee7b4d9cf
David Linkowski <david.linkowski@gmail.com>
2026-01-02 15:47:23 -0700

    Added some more efficiency changes to the solution.

--- top-interview-questions/array/remove-duplicates-from-sorted-array.js
+++ top-interview-questions/array/remove-duplicates-from-sorted-array.js
@@ -13,12 +13,19 @@ I am thinking a two-pointer solution could solve this problem. The left pointer 
   - increment l
   - swap elems at r and l indicies
 No matter what, we will always increment r.
-This patter will repeat until r >= the array length.
+This pattern will repeat until r >= the array length.
 
-In this way, we will always  track the end of the output array with l and iterate through all elements with r.
+In this way, we will always  track the end of the output array with l (k = l+1) and iterate through all elements with r.
 
 IMPROVEMENT IDEAS:
-In an array will already all non-decreasing integers, we will perform unnecessary swaps at every iteration (i.e. will will be swapping at the same index). Since swaps are more expensive than checks, we could add a conditional that will swap if and only if l !== r.  This would save use some swaps.
+In an array that doesn't have duplicates, we will perform unnecessary swaps at every iteration (i.e. will will be swapping at the same index). Since swaps are more expensive than checks, we could add a conditional that will swap if and only if l !== r.  This would save use some swaps and additional memory.
+
+After futher consideration, we don't even nee to swap, we can just set the value at index l to the value at index r since we don't care about losing the duplicate value at all (we ignore all values after index k at the end)
+
+COMPLEXITY:
+Time: O(n) proportional to the size of the array
+Space 0(1)
+
 */
 
 /**
@@ -26,17 +33,15 @@ In an array will already all non-decreasing integers, we will perform unnecessar
  * @return {number}
  */
 var removeDuplicates = function (nums) {
-  n = nums.length;
+  let n = nums.length;
   if (n <= 1) return n;
-  l = 0;
-  r = l + 1;
+  let l = 0;
+  let r = l + 1;
 
   while (r <= n - 1) {
     if (nums[l] != nums[r]) {
       l++;
-      let temp = nums[l];
       nums[l] = nums[r];
-      nums[r] = temp;
     }
     r++;
   }
@@ -45,7 +50,6 @@ var removeDuplicates = function (nums) {
 
 function main() {
   let nums = [1, 2, 2, 3, 4, 4, 6];
-  nums = [1, 3, 5, 7, 9, 11];
   let rslt = removeDuplicates(nums);
   console.log(rslt);
 }
