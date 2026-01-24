1d7abc693c33dfba5e98a942c9b3b0e92111fb37
David Linkowski <david.linkowski@gmail.com>
2026-01-24 14:13:21 -0500

    Finished solution to rotate image

--- top-interview-questions/array/easy/rotate-img.js
+++ top-interview-questions/array/easy/rotate-img.js
@@ -6,11 +6,39 @@ You are given an n x n 2D matrix representing an image, rotate the image by 90 d
 You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
 */
 
+function transposeSqrMatrix(matrix) {
+  const n = matrix.length;
+  for (let i = 0; i < n; ++i) {
+    for (let j = i; j < n; ++j) {
+      if (i !== j) [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+    }
+  }
+}
+
+function reverseArr(arr) {
+  const n = arr.length;
+  let r = n - 1;
+  let l = 0;
+  while (l <= r) {
+    [arr[l], arr[r]] = [arr[r], arr[l]];
+    l++;
+    r--;
+  }
+}
+
 /**
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
-var rotate = function (matrix) {};
+var rotate = function (matrix) {
+  const n = matrix.length;
+  for (let i = 0; i < n; ++i) {
+    for (let j = i; j < n; ++j) {
+      if (i !== j) [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+    }
+    reverseArr(matrix[i]);
+  }
+};
 
 function main() {
   let matrix = [
@@ -23,6 +51,9 @@ function main() {
     [8, 5, 2],
     [9, 6, 3],
   ];
+  console.log(matrix);
+  rotate(matrix);
+  console.log(matrix);
   console.assert(
     JSON.stringify(matrix) === JSON.stringify(out),
     "Matrix rotation failed",
