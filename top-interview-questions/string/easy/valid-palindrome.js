1e8df535f826b670e63951bfd6fb8821ea584628
David Linkowski <david.linkowski@gmail.com>
2026-01-26 15:14:38 -0500

    Started solution to palindrome in js

---
+++ top-interview-questions/string/easy/valid-palindrome.js
@@ -0,0 +1,20 @@
+/// Valid Palidrome ///
+
+/*
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+NOTES:
+
+This can be solved with a two-pointer system, similar to reverse string.  Could even use the reverse string solution in here to check if the given string is the same both forward and backward. We will use the two-pointer solution in isolation. 
+*/
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {};
+
+function main() {}
+
+main();
